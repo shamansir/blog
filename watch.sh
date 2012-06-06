ffa65673b914a86e0aa5e6a963a62f4b05667a23
Ulric Wilfred <shaman.sir@gmail.com>
2012-06-07 00:18:54 +0400

    really cool watch script

---
+++ watch.sh
@@ -0,0 +1,21 @@
+ROOT_DIR=`pwd`
+DEF_LANG=en
+SEC_LANG=ru
+TMP_DIR=.tmp
+
+cd $ROOT_DIR
+rm -Rf ./$TMP_DIR
+mkdir -p ./$TMP_DIR
+mkdir ./$TMP_DIR/_site
+mkdir ./$TMP_DIR/_site/$SEC_LANG
+mkdir ./$TMP_DIR/_subsites
+mkdir ./$TMP_DIR/_subsites/$SEC_LANG
+cp -R ./$DEF_LANG/* ./$TMP_DIR
+cp -R ./shared/* ./$TMP_DIR
+cp -R ./$SEC_LANG/* ./$TMP_DIR/_subsites/$SEC_LANG
+mynt gen -f ./$TMP_DIR ./$TMP_DIR/_site
+mynt gen -f ./$TMP_DIR/_subsites/$SEC_LANG ./$TMP_DIR/_site/$SEC_LANG
+mynt watch -f --base-url=/ ./$TMP_DIR ./$TMP_DIR/_site &
+mynt watch -f --base-url=/ru/ ./$TMP_DIR/_subsites/$SEC_LANG ./$TMP_DIR/_site/$SEC_LANG &
+mynt serve --base-url=/ ./$TMP_DIR/_site
+
